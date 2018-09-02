99710c8f7379e6bef00972b19b036c9cb62ba563
iking <iking31415@gmail.com>
2018-09-02 12:03:36 -0400

    Updates made to project Comments

--- HelloWorld.API/Controllers/HWController.cs
+++ HelloWorld.API/Controllers/HWController.cs
@@ -8,9 +8,16 @@ using System.Web.Http;
 
 namespace HelloWorld.API.Controllers
 {
+    /// <summary>
+    /// RESTful api controller for the HWModel object 
+    /// </summary>
     public class HWController : ApiController
     {
         List<HWModel> model;
+
+        /// <summary>
+        /// Instantiates the Model for the api and populates 2 values
+        /// </summary>
         public HWController()
         {
             model = new List<HWModel>()
@@ -27,29 +34,47 @@ namespace HelloWorld.API.Controllers
                 }
             };
         }
-
+ 
+        /// <summary>
+        /// Returns a list of all HWModel objects currently in memory
+        /// </summary>
         public IEnumerable<HWModel> GetMessages()
         {
             return model;
         }
 
+        /// <summary>
+        /// Returns a single HWModel object
+        /// </summary>
+        /// <param name="id">The ID of teh object to return</param>
         public HWModel GetMessage(int id)
         {
             return model.SingleOrDefault(m => m.ID == id);
         }
 
+        /// <summary>
+        /// Updates the HWObject of the selected ID
+        /// </summary>
+        /// <param name="id">The ID of teh object to update</param>
+        /// <param name="message">the message to be updated</param>
         public void PutMessage(int id, [FromBody]string message)
         {
             HWModel modelToEdit = model.SingleOrDefault(m => m.ID == id);
             
             modelToEdit.Message = message;
         }
-
+        /// <summary>
+        /// Adds a new HWModel Object to the list of objects
+        /// </summary>
+        /// <param name="modelToAdd">Object of type HWModel to be added to the list</param>
         public void PostMessage(HWModel modelToAdd)
         {
             model.Add(modelToAdd);
         }
-
+        /// <summary>
+        /// Deletes the selected object of ID "id" from teh list
+        /// </summary>
+        /// <param name="id">The ID of the selected object</param>
         public void DeleteMessage(int id)
         {
             HWModel modelToDelete = model.SingleOrDefault(m => m.ID == id);
